4e3bcd81765ecc38ea60f91d5d66ce29af79ac37
Yarrou <konic2884@gmail.com>
2022-08-04 11:43:45 +0200

    to create a secure connection, certificates of the "pem" type are used. This eliminates the need to convert to the p12 type.

--- src/main/java/site/alexkononsol/siteAKS/SiteAksApplication.java
+++ src/main/java/site/alexkononsol/siteAKS/SiteAksApplication.java
@@ -1,12 +1,16 @@
 package site.alexkononsol.siteAKS;
 
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class SiteAksApplication {
 
 	public static void main(String[] args) {
+		Security.addProvider(new PemKeyStoreProvider());
 		SpringApplication.run(SiteAksApplication.class, args);
 	}
 
