65bfbc1dddfe7d902e587216c932fbaff2b5d936
yarrou <38997244+yarrou@users.noreply.github.com>
2022-08-15 21:38:04 +0200

    Update ReloadableServletFactory.java
    changed ports

--- src/main/java/site/alexkononsol/siteAKS/reloadingSertificates/ReloadableServletFactory.java
+++ src/main/java/site/alexkononsol/siteAKS/reloadingSertificates/ReloadableServletFactory.java
@@ -45,9 +45,9 @@ public class ReloadableServletFactory extends TomcatServletWebServerFactory {
     private static Connector getHttpConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(80);
         connector.setSecure(false);
-        connector.setRedirectPort(8443);
+        connector.setRedirectPort(443);
         return connector;
     }
 }
